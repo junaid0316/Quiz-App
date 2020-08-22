2d2b1d57331bbd98b7731bf3651768369be40e8d
Emmadsadiq01 <junaid0316>
2020-08-22 15:11:35 +0500

    Quiz App

---
+++ fb.js
@@ -0,0 +1,9 @@
+function savedata(){
+    var name=document.getElementById("name")
+    var pass=document.getElementById("pass")
+    var student ={
+        name: name.value,
+        pass: pass.value
+    }
+    firebase.database().ref('student').push(student)
+}
